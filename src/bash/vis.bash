b9fe45057d3e2bd593ae8553218198b633aa049e
hari64 <hari64boli64@gmail.com>
2022-09-15 16:55:28 +0900

    update vis.bash

--- src/bash/vis.bash
+++ src/bash/vis.bash
@@ -1,3 +1,11 @@
+set -e
+
+read -p "Do compile? [y] :" YN
+if [ $YN = "y" ]; then
+    echo "now compiling..."
+    /usr/bin/g++ -fdiagnostics-color=always ./src/cpp/RobotPainter.cpp -o ./src/cpp/RobotPainter -std=c++17 -Dhari64 -Wall -Wextra -O3
+fi
+
 export DISPLAY=:0.0
 YELLOW="\033[33m"
 RESET="\033[0m"
